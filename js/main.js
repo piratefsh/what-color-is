c97b757e353f72611100fb1a6cc63ecffd9e821c
Sher Minn Chong <sherminn.chong@gmail.com>
2015-09-10 10:58:09 -0400

    Make pretty

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+google.load('search', '1');
 
+
+var imageSearch, searchTerm;
+
+var searchTermField = document.getElementById('search-term');
+var btnExecuteSearch = document.getElementById('btn-search');
+searchTermField.onclick = function(e){
+    imageSearch.execute(searchTerm);
+
+       
+}
+
+function onSearchAPILoad(){
+    // create image search
+    imageSearch = new google.search.ImageSearch();
+    imageSearch.setSearchCompleteCallback(this, onSearchComplete, null);
+    btnExecuteSearch.disabled = false;
+}
+
+function onSearchComplete(){
+    var results = imageSearch.results;
+    if(results){
+        console.log(results);
+    }
+}
